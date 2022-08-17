f25f203c8ec8729dc555b8d451fd37b1a32f19b6
Carlos Salame <carlosa@Carloss-MacBook-Pro.local>
2022-08-16 20:00:00 -0500

    fixed seed folder and modified some values on migration folders that were giving some errors

--- src/db/migrations/20220815165749_reviewsTable.js
+++ src/db/migrations/20220815165749_reviewsTable.js
@@ -2,12 +2,16 @@
 exports.up = function(knex) {
     return knex.schema.createTable("reviews", (table) => {
         table.increments("review_id").primary();
-        table.string("content");
+        table.text("content");
         table.integer("score");
+
+        table.integer("movie_id").unsigned().notNullable();
         table.foreign("movie_id")
         .references("movie_id")
         .inTable("movies")
         .onDelete("cascade");
+
+        table.integer("critic_id").unsigned().notNullable();
         table.foreign("critic_id")
     .references("critic_id")
     .inTable("critics")
