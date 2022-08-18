f8e1b8b9048e928e9e4d7fbfcfe61ef20e31e4f9
Carlos Salame <carlosa@Carloss-MacBook-Pro.local>
2022-08-18 18:41:43 -0500

    Fixed migration with heroku

--- knexfile.js
+++ knexfile.js
@@ -7,14 +7,17 @@ require("dotenv").config();
 //   DATABASE_URL = "postgresql://postgres@localhost/postgres",
 // } = process.env;
 
-const { DATABASE_URL = "postgresql://postgres@localhost:5432/postgres",
-} = process.env;
-
+const { DATABASE_URL = "postgresql://postgres@localhost:5432/postgres", PRODUCTION_DATABASE_URL } =
+  process.env;
+const URL =
+  process.env.NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -26,7 +29,10 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      connectionString: URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
